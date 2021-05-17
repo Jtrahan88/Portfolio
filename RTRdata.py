947102b429cdab337e770134da2bf0696d6ccb38
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-05-17 12:04:30 -0700

    RTR Tickets

--- RTRdata.py
+++ RTRdata.py
@@ -1,4 +1,3 @@
-
 # TODO:
 # FROM SS: ---> SIDE BY SIDE COMPARISON
 #  GET TOTAL VEHICLE COUNTS AND VEHICLES REMOVED COUNTS FOR EACH COUNTY
@@ -21,35 +20,32 @@ import numpy as np
 from tkinter import Tk
 from tkinter.filedialog import askopenfile
 
-# TODO: make this so I do not have to manually enter it. This Sheet is constanly changing. Maybe webscraping?
 
-#TODO: make GUI once everything else is complete.
-#     labelThisVarDifferent = askopenfile(title='Select File')  # shows dialog box and return the path
-#     # Tk.withdraw()
 
 
 # RTR Workbook variable
 fileNameRTR = "cCalRecycle_NorthBranch_DataManagerTicketExport.xlsx"
 
 print("Opening Vehicle check program.....")
-dfRTR = pd.read_excel(fileNameRTR, engine="openpyxl") # opens RTR unit rate ticket data refered to as dfRTR
-pd.options.display.width= None #only way to display all columns and rows for my data set 2500cols x 119rows
+dfRTR = pd.read_excel(fileNameRTR, engine="openpyxl")  # opens RTR unit rate ticket data refered to as dfRTR
+pd.options.display.width = None  # only way to display all columns and rows for my data set 2500cols x 119rows
 pd.set_option('display.max_rows', 3000)
 pd.set_option('display.max_columns', 3000)
 pd.options.display.max_columns = None
-#TODO: how to make open and close file more automated*
+# TODO: how to make open and close file more automated*
 
 
 # TODO:
 #  filter the data for service codes "4" (Go Ahead) and "C04" (Go Back) and for voild ==False only
-#dfRTR = dfRTR[["Zone Name", "End Time", "Is Void", "Ticket Notes", "Service Code", "Unit Count", "Disposal Monitor Name", "Addr No",
-               # "Addr St", "Ticket Number"]]
-#Varaibles for conditons
+# dfRTR = dfRTR[["Zone Name", "End Time", "Is Void", "Ticket Notes", "Service Code", "Unit Count", "Disposal Monitor Name", "Addr No",
+# "Addr St", "Ticket Number"]]
+# Varaibles for conditons
 # voidFalse = ['Is Void'] == False
 # serviceCode4 = ['Service Code'] == '4'
 # serviceCodeC04 = ['Service Code']== 'CO4'
 # serviceCode = dfRTR["Service Code"].isin(["CO4 ", "4"])]
-filt = dfRTR[(dfRTR['Is Void'] == False) & (dfRTR["Service Code"].isin(["CO4 ", "4"]))] # .isin was the key for this. MAKE A YOUTUBE ON THIS 5+hrs to figure this out. 
+filt = dfRTR[(dfRTR['Is Void'] == False) & (dfRTR["Service Code"].isin(
+    ["CO4 ", "4"]))]  # .isin was the key for this. MAKE A YOUTUBE ON THIS 5+hrs to figure this out.
 
 filt.to_excel('VecTest.xlsx')
 
@@ -62,25 +58,8 @@ filt.to_excel('VecTest.xlsx')
 # dfRTR.to_excel("TestRTR.xlsx", index=False) # RTR save to an excel file if needed
 
 
-
-
-
 # TODO: Function for otal vehicle counts afeter checks. Graph for display*. ******************We need to do comparisions with/before this part******************.
 
-#TODO: Make a function that; get all the columns we need and compare to other excel sheet(s)
+# TODO: Make a function that; get all the columns we need and compare to other excel sheet(s)
 # Rearrange column orders for RTR Data:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
