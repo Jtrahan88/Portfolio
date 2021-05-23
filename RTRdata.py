3d98556f8d58a2db2671a3df3e562eebef1df7ce
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-05-22 21:43:02 -0700

    Update RTRdata.py

--- RTRdata.py
+++ RTRdata.py
@@ -1,3 +1,4 @@
+
 # TODO:
 # FROM SS: ---> SIDE BY SIDE COMPARISON
 #  GET TOTAL VEHICLE COUNTS AND VEHICLES REMOVED COUNTS FOR EACH COUNTY
@@ -15,56 +16,72 @@
 
 import pandas as pd
 import openpyxl
-from recordlinkage.preprocessing import clean, phonetic
 import matplotlib.pyplot as plt
 import numpy as np
-# from tkinter import Tk
-# from tkinter.filedialog import askopenfile
+from tkinter import Tk
+from tkinter.filedialog import askopenfile
 
+# TODO: make this so I do not have to manually enter it. This Sheet is constanly changing. Maybe webscraping?
 
+#TODO: make GUI once everything else is complete.
+#     labelThisVarDifferent = askopenfile(title='Select File')  # shows dialog box and return the path
+#     # Tk.withdraw()
 
 
 # RTR Workbook variable
 fileNameRTR = "cCalRecycle_NorthBranch_DataManagerTicketExport.xlsx"
 
 print("Opening Vehicle check program.....")
-dfRTR = pd.read_excel(fileNameRTR, usecols=["Zone Name", "End Time", "Is Void", "Ticket Notes", "Service Code",
-                                            "Unit Count", "Disposal Monitor Name","Addr No","Addr St", "Ticket Number"])
-df= dfRTR.set_index('Zone Name') # make index the Zone Name
-
-# Display all Dataframe with no truncating
-pd.options.display.width = None  # only way to display all columns and rows for my data set 2500cols x 119rows
+dfRTR = pd.read_excel(fileNameRTR,usecols=["Zone Name", "End Time", "Is Void", "Ticket Notes",
+                                           "Service Code", "Unit Count", "Disposal Monitor Name", "Addr No",
+               "Addr St", "Ticket Number"], index_col="Zone Name")
+pd.options.display.width= None #only way to display all columns and rows for my data set 2500cols x 119rows
 pd.set_option('display.max_rows', 3000)
 pd.set_option('display.max_columns', 3000)
 pd.options.display.max_columns = None
-# TODO: how to make open and close file more automated*
-# print(dfRTR.columns) # get all column  headings
+#TODO: how to make open and close file more automated*
 
 
+# TODO:
+#  filter the data for service codes "4" (Go Ahead) and "C04" (Go Back) and for voild ==False only
+  # .isin was the key for this. MAKE A YOUTUBE ON THIS 5+hrs to figure this out.
 
-# print(df.dtypes.value_counts())
-def RTRmehSetup():
-    df.to_excel("RTR Data for vecs.xlsx")
-
-RTRmehSetup()
+# dfRTR.to_csv('Test.csv')
+# dfRTR.replace("CO4 ","CO4")
+# dfRTR.replace("4 ", "4")
 
 
-def RTRDataSetUp(df): #TODO: doesnt work.populate correctly but deletes to many rows because of the "4"
-    filt = df[(df['Is Void'] == False) & (df["Service Code"].isin(["CO4 ", "4"]))]  # .isin was the key for this. MAKE A YOUTUBE ON THIS 5+hrs to figure this out.
-    # filt.to_excel('RTRdata for vec checks.xlsx')
-RTRDataSetUp(df)
+dfRTR['Service Code'] = dfRTR['Service Code'].str.replace(r'\W', "")
+filt = dfRTR[(dfRTR['Is Void'] != True) & dfRTR["Service Code"].isin(["CO4", "4"])]
+filt.to_excel('VecTest.xlsx')
 
-#  filter out(delete) all True vales under the Is Void column
 #  Figure out the weird APNs(In Zone Name), by hard coding, what they need to look like.
-#  Examples: 0203200600 should = 020-320-06-00 | 0203300500_#105 should = 020-330-05-00_#105
+#           Examples: 0203200600 should = 020-320-06-00 | 0203300500_#105 should = 020-330-05-00_#105
 #  The rest of the APNs by adding dashes and -000 for example 123456789 need to look like 123-456-789-000
 
 
 # dfRTR.to_excel("TestRTR.xlsx", index=False) # RTR save to an excel file if needed
 
 
+
+
+
 # TODO: Function for otal vehicle counts afeter checks. Graph for display*. ******************We need to do comparisions with/before this part******************.
 
-# TODO: Make a function that; get all the columns we need and compare to other excel sheet(s)
+#TODO: Make a function that; get all the columns we need and compare to other excel sheet(s)
 # Rearrange column orders for RTR Data:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
