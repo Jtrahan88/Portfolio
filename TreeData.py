734ac3e6e265815e03ed004e1c31d2c2cb11f511
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-05-23 18:29:56 -0700

    Update TreeData.py

--- TreeData.py
+++ TreeData.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-fileName = 'North Branch CA Wildfires - Parcel Issues.xlsx'
+fileName = 'North Branch CA Wildfires - Parcel Issues.csv'
 
-treeDf = pd.read_excel(fileName, usecols= ['APN','Tree Survey Date', 'House Number', 'Street Address',
+treeDf = pd.read_csv(fileName, usecols= ['APN','Tree Survey Date', 'House Number', 'Street Address',
                                            'Are there hazards present on the property?', 'County','Number of Trees',
                                            'Crew Leader', 'Arborist'], index_col='APN')
 
-print(treeDf.columns)
+treeDf.to_excel("Test Csv File.xlsx")
