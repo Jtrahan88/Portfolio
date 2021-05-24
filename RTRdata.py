8a142875fe0110ec1cdeb05d416bb40c6b0b38fb
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-05-24 14:25:02 -0700

    Update RTRdata.py

--- RTRdata.py
+++ RTRdata.py
@@ -1,9 +1,12 @@
 
-# TODO:
-# FROM SS: ---> SIDE BY SIDE COMPARISON
-#  GET TOTAL VEHICLE COUNTS AND VEHICLES REMOVED COUNTS FOR EACH COUNTY
-#  WE NEED TO ALSO SHOW STREET #, ADDRESS, DEBRIS REMOVAL DATE
+#TODO:
+# Filter for columns needed
+# FROM SS: ---> SIDE BY SIDE COMPARISON with RTR
+#  Graph: GET TOTAL VEHICLE COUNTS AND VEHICLES REMOVED COUNTS FOR EACH COUNTY-make my function a double bar graph
 #  Show any mismatch(highlight them, make them pop out in a table with reason, etc)
+#  make GUI once everything else is complete.
+#  labelThisVarDifferent = askopenfile(title='Select File')  # shows dialog box and return the path
+#  Tk.withdraw()
 
 
 # TODO: Documentation locations:
@@ -11,41 +14,34 @@
 # matplotlib: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.html
 # openpyxl:
 # numpy:
-#  RTR Data vehicle aspects that will be used. only this function will have this.
-# "Zone Name", "End Time", "Is Void", "Ticket Notes", "Service Code", "Unit Count", "Disposal Monitor Name", "Addr No", "Addr St", "Ticket Number"
+#  Smart Sheets vehicle aspects that will be used. only this function will have this. APN = index inplace is False
+# 'APN'(made index not needed), 'Street #', 'Street Name', 'Number of Vehicles', 'Number of Vehicles Removed', 'Debris Finish', 'County'
 
 import pandas as pd
-import openpyxl
+# import openpyxl
 import matplotlib.pyplot as plt
 import numpy as np
-from tkinter import Tk
-from tkinter.filedialog import askopenfile
-
-# TODO: make this so I do not have to manually enter it. This Sheet is constanly changing. Maybe webscraping?
-
-#TODO: make GUI once everything else is complete.
-#     labelThisVarDifferent = askopenfile(title='Select File')  # shows dialog box and return the path
-#     # Tk.withdraw()
+# from tkinter import Tk
+# from tkinter.filedialog import askopenfile
 
 
-# RTR Workbook variable
-fileNameRTR = "cCalRecycle_NorthBranch_DataManagerTicketExport.xlsx"
+#TODO: SS Excel Name: make this so I do not have to manually enter it. This Sheet is constanly changing. Maybe webscraping?
+# Smart Sheets Workbook variable
+fileNameSS = "Northern Branch Phase II Debris Removal Ops.xlsx"
+
 
 print("Opening Vehicle check program.....")
-dfRTR = pd.read_excel(fileNameRTR,usecols=["Zone Name", "End Time", "Is Void", "Ticket Notes",
-                                           "Service Code", "Unit Count", "Disposal Monitor Name", "Addr No",
-               "Addr St", "Ticket Number"], index_col="Zone Name")
+dfSS = pd.read_excel(fileNameSS,usecols= ['APN','Street #', 'Street Name','Debris Finish', 'Number of Vehicles',
+                                          'Number of Vehicles Removed',  'County'], index_col='APN')
 pd.options.display.width= None #only way to display all columns and rows for my data set 2500cols x 119rows
 pd.set_option('display.max_rows', 3000)
 pd.set_option('display.max_columns', 3000)
-pd.options.display.max_columns = None
+pd.options.display.max_columns= None
 #TODO: how to make open and close file more automated*
-
-dfRTR.to_excel("RTR Vec Cols.xlsx")
-
+#  # df.reset_index(inplace=True) #This will reset the index back to numbers
 
 
-
+# dfSS.to_excel("SS Cols for Vecs.xlsx")
 
 
 
@@ -53,35 +49,36 @@ dfRTR.to_excel("RTR Vec Cols.xlsx")
 
 
 
-#TODO: works in CVS but formulas do not match once made back into a excel. 1 != 1 for odd reasons unknown at this time.
-# Exmaple =A1=B1 if both cells show 1 and 1 that formula will say false. 
-def getRTRVecCols(dfRTR):
-    dfRTR['Service Code'] = dfRTR['Service Code'].str.replace(r'\W', "") #TODO: Learn more about Regex!!!
-    filt = dfRTR[(dfRTR['Is Void'] != True) & dfRTR["Service Code"].isin(["CO4", "4"])]
-    filt.to_excel('RTR Vec Cols.xlsx')
-
-# getRTRVecCols(dfRTR)
-
-#  Figure out the weird APNs(In Zone Name), by hard coding, what they need to look like.
-#           Examples: 0203200600 should = 020-320-06-00 | 0203300500_#105 should = 020-330-05-00_#105
-#  The rest of the APNs by adding dashes and -000 for example 123456789 need to look like 123-456-789-000
-
-
-# dfRTR.to_excel("TestRTR.xlsx", index=False) # RTR save to an excel file if needed
 
 
 
 
 
-# TODO: Function for otal vehicle counts afeter checks. Graph for display*. ******************We need to do comparisions with/before this part******************.
 
-#TODO: Make a function that; get all the columns we need and compare to other excel sheet(s)
-# Rearrange column orders for RTR Data:
+#TODO: get graph to only count numbers, and number >0 ONLY!
+def getTotalCounts(dfSS): # this function is for getting counts by any way you need.
+    county = dfSS.groupby(['County']).count()  # this will put "COUNTY"on Y-Axis and counts all the values from other columns
+    counts = county.loc[:,"Number of Vehicles Removed"]  # left side :  all rows , right side: is the column we want to look at. This is how we chose what we want to ount up
+    # counts.to_excel("test3.xlsx") # if we use county we get counts for entier data set columns by county, if we use counts.to_excel() we only get vec removed column.
+
+    # TODO: Make a graph based on above.
+    counts.plot(kind='bar', x='county', y='counts') # this makes our bar chart from the data.
+    # #TODO: Add number to bar charts
+    for i in range(len(county)): # for loop for each element in the lenth of counties, display the height of each row center and at the top of the repectiv bar chart.
+        plt.text(i, counts[i], counts[i], ha='center', va='bottom') #i for each index. [i] for the height of counts(the bar/number og vehicles removed)
+        # the 2nd count[i] is for numbers but we can put text there
+
+    plt.title('Total Vehicle Removal Counts By County') #title the bar chart
+    plt.xlabel("Counties")  # labels the X-Axis on our graph
+    plt.ylabel('Vehicles Removed')  # labels the Y-Axis on our graph
+    plt.legend()  # add a legend for our graph
+    # plt.xticks([1,2,3]) # change to x tick values and how much they go or down by
+    # plt.yticks([1,2,3]) # change to y tick values and how much they go or down by
+    print(dfSS[["County", "Number of Vehicles Removed"]].groupby("County").count())
+    plt.show()  # show or print out the graph to view
 
 
-
-
-
+getTotalCounts(dfSS) # test function
 
 
 
