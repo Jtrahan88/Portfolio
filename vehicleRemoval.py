9ebd1c9b3ded469ed31042544055b66ab397a5be
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-05-23 18:30:42 -0700

    Update vehicleRemoval.py

--- vehicleRemoval.py
+++ vehicleRemoval.py
@@ -18,49 +18,32 @@
 # 'APN'(made index not needed), 'Street #', 'Street Name', 'Number of Vehicles', 'Number of Vehicles Removed', 'Debris Finish', 'County'
 
 import pandas as pd
-import openpyxl
-import matplotlib.pyplot as plt
+# import openpyxl
+# import matplotlib.pyplot as plt
 import numpy as np
 # from tkinter import Tk
 # from tkinter.filedialog import askopenfile
 
 
-#TODO: SS Excel Name:
-# make this so I do not have to manually enter it. This Sheet is constanly changing. Maybe webscraping?
+#TODO: SS Excel Name: make this so I do not have to manually enter it. This Sheet is constanly changing. Maybe webscraping?
 # Smart Sheets Workbook variable
-fileNameSS = "Northern Branch Phase II Debris Removal Ops (52).xlsx"
+fileNameSS = "Northern Branch Phase II Debris Removal Ops 05-18-2021.csv"
 
 
 print("Opening Vehicle check program.....")
-dfSS = pd.read_excel(fileNameSS, usecols=['APN','Street #', 'Street Name','Debris Finish', 'Number of Vehicles',
+dfSS = pd.read_csv(fileNameSS,usecols= ['APN','Street #', 'Street Name','Debris Finish', 'Number of Vehicles',
                                           'Number of Vehicles Removed',  'County'], index_col='APN')
- # inplace=True to make it permanent
-
-#only way to display all columns and rows for my data set 2500 rows x 119 columns
-pd.options.display.width= None
+pd.options.display.width= None #only way to display all columns and rows for my data set 2500cols x 119rows
 pd.set_option('display.max_rows', 3000)
 pd.set_option('display.max_columns', 3000)
 pd.options.display.max_columns= None
 #TODO: how to make open and close file more automated*
-#  # dfSS.reset_index(inplace=True) #This will reset the index back to numbers
-
-#TODO: filtering data
+#  # df.reset_index(inplace=True) #This will reset the index back to numbers
 
 
-# TODO: Save sheet to excel file for us
-def vehicleCols(dfSS):
-    dfSS.to_excel("VehicleColumns.xlsx")  # saves to a new excel file
-    return dfSS
-vehicleCols(dfSS)
+# dfSS.to_excel("Test the csv file.xlsx")
 
-
-
-#TODO: V or xlookups
-
-
-
-
-# TODO: Grahphing (WIP)
+# TODO: Function for total vehicle counts after checks. Graph for display*. ******************We need to do comparisions with/before this part******************.
 def getTotalCounts(dfSS): # this function is for getting counts by any way you need.
     county = dfSS.groupby(['County']).count()  # this will put "COUNTY"on Y-Axis and counts all the values from other columns
     counts = county.loc[:,"Number of Vehicles Removed"]  # left side :  all rows , right side: is the column we want to look at. This is how we chose what we want to ount up
@@ -72,23 +55,18 @@ def getTotalCounts(dfSS): # this function is for getting counts by any way you n
     for i in range(len(county)): # for loop for each element in the lenth of counties, display the height of each row center and at the top of the repectiv bar chart.
         plt.text(i, counts[i], counts[i], ha='center', va='bottom') #i for each index. [i] for the height of counts(the bar/number og vehicles removed)
         # the 2nd count[i] is for numbers but we can put text there
-    #TODO: Lables for graph
+
     plt.title('Total Vehicle Removal Counts By County') #title the bar chart
     plt.xlabel("Counties")  # labels the X-Axis on our graph
     plt.ylabel('Vehicles Removed')  # labels the Y-Axis on our graph
     plt.legend()  # add a legend for our graph
     # plt.xticks([1,2,3]) # change to x tick values and how much they go or down by
     # plt.yticks([1,2,3]) # change to y tick values and how much they go or down by
-
+    print(dfSS[["County", "Number of Vehicles Removed"]].groupby("County").count())
     plt.show()  # show or print out the graph to view
-    print(dfSS[["County", "Number of Vehicles Removed"]].groupby("County").count())  # gets total vehicle counts by county display on screen for copy/check(optional)
 
 
-# getTotalCounts(dfSS) # test function
-
-
-
-
+getTotalCounts(dfSS) # test function
 
 
 
