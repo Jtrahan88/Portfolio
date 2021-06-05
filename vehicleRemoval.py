48473013d695f9f8eca808935b26dc29aa0e1864
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-06-05 13:44:08 -0700

    Update vehicleRemoval.py

--- vehicleRemoval.py
+++ vehicleRemoval.py
@@ -32,16 +32,28 @@ fileNameSS = "Northern Branch Phase II Debris Removal Ops.xlsx"
 
 print("Opening Vehicle check program.....")
 dfSS = pd.read_excel(fileNameSS,usecols= ['APN','Street #', 'Street Name','Debris Finish', 'Number of Vehicles',
-                                          'Number of Vehicles Removed',  'County'], index_col='APN')
-pd.options.display.width= None #only way to display all columns and rows for my data set 2500cols x 119rows
+                                          'Number of Vehicles Removed',  'County']) # index_col='APN' do this later after futher editing APNS
+#only way to display all columns and rows for my data set
 pd.set_option('display.max_rows', 3000)
 pd.set_option('display.max_columns', 3000)
 pd.options.display.max_columns= None
+pd.options.display.width= None
 #TODO: how to make open and close file more automated*
+
+# removes all dashes then joins that together with no spaces for full APN & remove extras zeros from APNS
+
+# dfSS["APN"] = dfSS["APN"].str.replace("-", "").str[:9] #TODO: Works but we need to skip over the Special APNs. Mayb use thing in a DICT?
+
+# make a dict for trinity and other counties that have special APNs so skip next step and leave as is.
+
+# remove extra zeros from APNS
+
+
+
 #  # df.reset_index(inplace=True) #This will reset the index back to numbers
 
 
-# dfSS.to_excel("SS Cols for Vecs.xlsx")
+dfSS.to_excel("SmartSheet Vehicle Set up.xlsx")
 
 
 
@@ -61,13 +73,15 @@ def getTotalCounts(dfSS): # this function is for getting counts by any way you n
     counts = county.loc[:,"Number of Vehicles Removed"]  # left side :  all rows , right side: is the column we want to look at. This is how we chose what we want to ount up
     # counts.to_excel("test3.xlsx") # if we use county we get counts for entier data set columns by county, if we use counts.to_excel() we only get vec removed column.
 
-    # TODO: Make a graph based on above.
+    # Make a graph based on above.
     counts.plot(kind='bar', x='county', y='counts') # this makes our bar chart from the data.
-    # #TODO: Add number to bar charts
+
+    #Add number to bar charts
     for i in range(len(county)): # for loop for each element in the lenth of counties, display the height of each row center and at the top of the repectiv bar chart.
         plt.text(i, counts[i], counts[i], ha='center', va='bottom') #i for each index. [i] for the height of counts(the bar/number og vehicles removed)
         # the 2nd count[i] is for numbers but we can put text there
 
+# Graph Add ons
     plt.title('Total Vehicle Removal Counts By County') #title the bar chart
     plt.xlabel("Counties")  # labels the X-Axis on our graph
     plt.ylabel('Vehicles Removed')  # labels the Y-Axis on our graph
@@ -78,7 +92,7 @@ def getTotalCounts(dfSS): # this function is for getting counts by any way you n
     plt.show()  # show or print out the graph to view
 
 
-getTotalCounts(dfSS) # test function
+# getTotalCounts(dfSS) # test function
 
 
 
