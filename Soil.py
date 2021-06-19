f0be351d7e9e88546a196d2e3b51135d8b3982e4
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-06-19 13:37:08 -0700

    Update Soil.py

--- Soil.py
+++ Soil.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-fileName = 'TT_Northern Branch_Soil.csv'
+fileName = 'TT_Northern Branch_Soil.xlsx'
 
-Soildf = pd.read_csv(fileName, header=1, usecols= ['APN','Street Number', 'Street Name','County', 'Date Collected',
+Soildf = pd.read_excel(fileName, header=1, usecols= ['APN','Street Number', 'Street Name','County', 'Date Collected',
                                                      'Number of Samples Collected', 'Date Shipped',
                                                      'State\'s Rec on Confirmation Sample','CalRecycle Final Decision']
                        , index_col='APN')
