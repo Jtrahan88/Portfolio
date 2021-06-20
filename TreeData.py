8afe239489a5c5b58db58795b14234a366b050d0
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-06-20 16:08:29 -0700

    Update TreeData.py

--- TreeData.py
+++ TreeData.py
@@ -1,7 +1,9 @@
 #TODO: LEft here due to maek need to add more tasks later
 
 import pandas as pd
+import time
 
+todaysDate = time.strftime("%m-%d-%y")
 tree_file = 'North Branch CA Wildfires - Parcel Issues.xlsx'
 
 df_tree = pd.read_excel(tree_file, sheet_name="TREE SURVEYS") # Tree Data
@@ -9,5 +11,5 @@ df_tree = pd.read_excel(tree_file, sheet_name="TREE SURVEYS") # Tree Data
 def tree_cols(df):
     df = df[["APN", "Tree Survey Date", "Number of Trees"]]
     df = df.set_index("APN")
-    df.to_excel("Tree data Columns" + todaysDate + ".xlsx")
+    df.to_excel("Tree data Columns " + todaysDate + ".xlsx")
 tree_cols(df_tree)
