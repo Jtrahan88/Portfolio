5b65fe298347a06cb4114c3c38588dc91ba66f08
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-06-20 15:24:46 -0700

    Update Mean_Days_Crews_on_Property.py

--- Mean_Days_Crews_on_Property.py
+++ Mean_Days_Crews_on_Property.py
@@ -20,11 +20,11 @@ def mean_crew_days_on_propert(df):
     start_date = input("Put your start day in this format 2021-05-01: ")
     end_day = input("Put your end day in this format 2021-06-19: ")
 
-    df.loc[:, ['Debris Start', 'Debris Finish']] = df[(df.loc[:, 'Debris Start'] > start_date) &
+    df.loc[:, ['Debris Start', 'Debris Finish']] = df[(df.loc[:, 'Debris Start'] >= start_date) &
                                                       (df.loc[:, 'Debris Finish'] <= end_day)]
     df.loc[:,  "Structural Status"] = df.loc[:, "Structural Status"].isin(status)
 
-    # duration - finish day minus start day
+    # Duration is the finish day minus start day
     df.loc[:, "Duration"] = df.loc[:, 'Debris Finish'] - df.loc[:, 'Debris Start']
 
     # convert 'duration' column to a float instead of timedelta64[ns]
@@ -34,17 +34,21 @@ def mean_crew_days_on_propert(df):
     df = df[['APN', "Structural Status", 'County', 'Debris Crew', 'Debris Crew WO#', 'Debris Start',
              'Debris Finish', 'Duration']]
 
-    # RE for filtering
+    # RE for filtering for debris crews. Gets rid of extra text and fixes formatting issues leaving "crew # and number"
     df['Debris Crew WO#'] = df['Debris Crew WO#'].str.extract('(CREW ?# ?\d+)')
 
     # take out the spaces in 'Debris Crew WO#'
     df['Debris Crew WO#'] = df['Debris Crew WO#'].str.replace(" ", "")
 
-    # use only active crews
+    # use only active crews. Comment this line out to get all crews. Shortcut key for commenting out a line = CTRL + /
     df.loc[:, 'Debris Crew WO#'] = df[df.loc[:, 'Debris Crew WO#'].isin(active_crews)]
-    dffilt = df.groupby(['County', 'Debris Crew WO#'])[["Duration"]].mean()
-    dffilt.to_excel("Mean days on property.xlsx")
 
+    # group by county and dbres crew showing the means days on property for each crew.
+    dffilt = df.groupby(['County', 'Debris Crew WO#'])[["Duration"]].mean()
+
+    #saves an excel for emailing
+    dffilt.to_excel("Mean days on property.xlsx")
+    #prints it to the screen as well
     return df.groupby(['County', 'Debris Crew WO#'])[["Duration"]].mean().round(2)
 
 
