125a26dd3943b24ef8088685e801ae8a27396d9d
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-06-20 16:14:16 -0700

    made improvements on speed with using .loc

--- NorthernBranchAuditCheck.py
+++ NorthernBranchAuditCheck.py
@@ -6,9 +6,9 @@ import time
 
 todaysDate = time.strftime("%m-%d-%y") # to add dates to our file
 
-#File name variable
+#File name variable TODO: make GUI for selecting eah file name.
 smartsheets_file = 'state & Field Tracker/Northern Branch Phase II Debris Removal Ops (1).xlsx'
-tree_file = 'state & Field Tracker/North Branch CA Wildfires - Parcel Issues.xlsx' # need to open specific sheet will not work as of now
+tree_file = 'state & Field Tracker/North Branch CA Wildfires - Parcel Issues.xlsx'
 sa_file = "state & Field Tracker/TT_North Branch_Site Assessment.xlsx"
 asb_file = 'state & Field Tracker/TT_North Branch_Asbestos.xlsx'
 soil_file = 'state & Field Tracker/TT_Northern Branch_Soil.xlsx'
@@ -22,40 +22,40 @@ df_soil = pd.read_excel(soil_file, header=1) # Soil Data
 
 
 # TODO: smart_sheets_cols(df) --> get colums ready fro VBA
-#  remove timefrom date time columns* Tricky due t0 they are objects and not Datetime maybe?
 def smart_sheets_cols(df):
+    df = df.copy()
     df = df[["APN", "County", "Structural Status", "Site Assessment", "Chimney", "NESHAP Walls",
              "Chimney Tipped", "Number of Vehicles", "Haz Trees Assessment", "# of Trees",
              "ASB Assessment", "ASB Results Received", "ASB Results", "ASB Abatement",
              "Number of Vehicles Removed", "Soil Sample"]]
+
     status = ("Ineligible", "Withdrawal") # double check the case sensitive aspects of these words
-    df = df[(~df["Structural Status"].isin(status))] # take out withdrawn and ineligible properties
-    # df["ASB Abatement"] = pd.to_datetime(df["ASB Abatement"])
-    # df["ASB Abatement"] = df["ASB Abatement"].dt.date
+    df = df[(~df.loc[:, "Structural Status"].isin(status))] # take out withdrawn and ineligible properties
     df = df.set_index("APN")
     df.to_excel("Smart Sheets Audit Column set up " + todaysDate + ".xlsx")
 smart_sheets_cols(df_ss)
 
+
 # TODO: tree_cols(df) ---> get columns ready for VBA
-#  will have to select a different sheet to edit. Not shet one!!
 def tree_cols(df):
-    df = df[["APN", "Tree Survey Date", "Number of Trees"]]
+    df = df.copy()
+    df = df.loc[:, ["APN", "Tree Survey Date", "Number of Trees"]]
     df = df.set_index("APN")
     df.to_excel("Tree data Columns" + todaysDate + ".xlsx")
 tree_cols(df_tree)
 
 # TODO: sa_cols(df) ---> get columns ready for VBA
-#  Sa has the columns on line two. Go back fins out how to remedy this again
 def sa_cols(df):
-    df = df[["APN", "Date Completed", "Automobiles"]]
+    df = df.copy()
+    df = df.loc[:, ["APN", "Date Completed", "Automobiles"]]
     df = df.set_index("APN")
     df.to_excel("SA data Columns " + todaysDate + ".xlsx")
 sa_cols(df_sa)
 
 # TODO: asb_cols(df) ---> get columns ready for VBA
-#  Sa has the columns on line two. Go back fins out how to remedy this again
 def asb_cols(df):
-    df = df[["APN", "Date Collected", "Tt Received Date", "Final Results", "Point Count Needed?", "Point Count Results",
+    df = df.copy()
+    df = df.loc[:, ["APN", "Date Collected", "Tt Received Date", "Final Results", "Point Count Needed?", "Point Count Results",
              "Chimney", "NESHAP Walls", "CHIM TIP (DATE)", "Chimney Finding",  "Asbestos Abatement Completed Date",
             "Chimney Abatement Completed Date", "No. of Samples"]]
     df = df.set_index("APN")
@@ -63,9 +63,9 @@ def asb_cols(df):
 asb_cols(df_asb)
 
 # TODO: soil_cols(df) ---> get columns ready for VBA
-#  Sa has the columns on line two. Go back fins out how to remedy this again
 def soil_cols(df):
-    df = df[["APN", "Date Collected"]]
+    df = df.copy()
+    df = df.loc[:, ["APN", "Date Collected"]]
     df = df.set_index("APN")
     df.to_excel("Soil data columns" + todaysDate + ".xlsx")
 soil_cols(df_soil)
