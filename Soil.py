ec1ed0376c36409a05fb92afb1a05da417884810
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-06-20 16:07:43 -0700

    Update Soil.py

--- Soil.py
+++ Soil.py
@@ -1,11 +1,16 @@
+#TODO: LEft here due to maek need to add more tasks later
+
 import pandas as pd
+import time
+todaysDate = time.strftime("%m-%d-%y")
+
+soil_file = 'state & Field Tracker/TT_Northern Branch_Soil.xlsx'
+df_soil = pd.read_excel(soil_file, header=1) # Soil Data
 
 
-fileName = 'TT_Northern Branch_Soil.xlsx'
-
-Soildf = pd.read_excel(fileName, header=1, usecols= ['APN','Street Number', 'Street Name','County', 'Date Collected',
-                                                     'Number of Samples Collected', 'Date Shipped',
-                                                     'State\'s Rec on Confirmation Sample','CalRecycle Final Decision']
-                       , index_col='APN')
-
-Soildf.to_excel("Test the csv file.xlsx")
+# TODO: soil_cols(df) ---> get columns ready for VBA
+def soil_cols(df):
+    df = df[["APN", "Date Collected"]]
+    df = df.set_index("APN")
+    df.to_excel("Soil data columns " + todaysDate + ".xlsx")
+soil_cols(df_soil)
