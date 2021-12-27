940039f84152e0e9e94a8a057701a6f4f3d56b9f
JQTs <69168405+Jtrahan88@users.noreply.github.com>
2021-12-27 08:24:44 -0800

    Opens browsers needed for h day

---
+++ Fire Projects/North and Central Dixie Fire/Start Dixie Day.py
@@ -0,0 +1,54 @@
+import webbrowser
+
+
+
+#Stack overflow
+webbrowser.open('https://stackoverflow.com/users/15725039/jqts')
+
+# PM QAQC CENTRAL DIVISION
+# webbrowser.open("https://docs.google.com/spreadsheets/d/1jEIyE8D55008vZWBYZz2VN27zAv4Ut3BSYeEtkQgff4/edit#gid=1359210055")
+
+# CENTRAL PLANNING TRACKER TT
+# webbrowser.open("https://docs.google.com/spreadsheets/d/1wF0GLNJG0dKTnkT0fEuC-kAIU4pzrr_IyhLnJ5iEZ7s/edit#gid=1409437831")
+
+# ROE Upload Tracker
+# webbrowser.open("https://docs.google.com/spreadsheets/d/19K2GtpVtsUwJkn_NTdfSK7zVOqInbh8-1mGQ6sk9yzg/edit#gid=1135516498")
+
+# Box
+webbrowser.open('https://app.box.com/folder/0')
+
+# Smart sheets
+webbrowser.open('https://app.smartsheet.com/b/home?dlp=%2Fsheets%2FQx3Qg8Qxj5fgVgWFqjJ8P77gJ4Vq6h6jrp7V3wr1&dlq=view%3Dgrid')
+
+# email
+webbrowser.open('https://outlook.office365.com/mail/inbox')
+
+# Google online Audit tracker
+webbrowser.open('https://docs.google.com/spreadsheets/d/1KAd19dY1dMOdB3s_l58bKFt4Zr1rjpI80Vtu9AK5u2A/edit#gid=1314238575')
+
+
+# Northern branch Smartsheets
+webbrowser.open('https://app.smartsheet.com/sheets/FXGjvG8cJXMvV5RGqRWVHc9JFHFG2mJmw73m8jg1?view=card&cardLevel=0&cardViewByColumnId=448610792957828')
+
+# Northern audit tracker
+webbrowser.open('https://docs.google.com/spreadsheets/d/1xzcEARVhONWXiEMV8FKbFADCpaU09HdLd-BuB-Y6MbQ/edit#gid=1314238575')
+
+# Asbestos Audits
+webbrowser.open('https://tetratechinc.sharepoint.com/teams/2021Fires-Asbestos/Lists/AsbestosTracker/AllItems.aspx?id=%2Fteams%2F2021Fires%2DAsbestos%2FLists%2FAsbestosTracker&newTargetListUrl=%2Fteams%2F2021Fires%2DAsbestos%2FLists%2FAsbestosTracker&viewpath=%2Fteams%2F2021Fires%2DAsbestos%2FLists%2FAsbestosTracker%2FAllItems%2Easpx&CT=1638891912516&OR=OWA%2DNT&CID=55bbafd0%2Dbdba%2D9035%2Dc630%2D319fa6ca5eaa')
+
+# tetra forms
+webbrowser.open('https://tetraforms.iformbuilder.com/exzact/dataViews.php')
+
+# ESL SQL master list
+# webbrowser.open('http://etss706gissql1/Reports/report/EMI/Fires2021/Asbestos%20Sample%20Status')
+
+
+
+
+
+
+
+
+
+
+
